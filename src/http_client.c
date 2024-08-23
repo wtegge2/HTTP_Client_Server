3197d85a253ca838cc17156272683acc169b7ef6
wtegge2 <wtegge2@illinois.edu>
2024-08-23 14:18:55 -0500

    added main files and makefile

---
+++ src/http_client.c
@@ -0,0 +1,495 @@
+
+/*
+** client.c -- a stream socket client demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+
+#define PORT "80" 
+
+#define BUFFER 4096
+
+#define MAXDATASIZE 5000
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+ if (sa->sa_family == AF_INET) {
+     return &(((struct sockaddr_in*)sa)->sin_addr);
+ }
+
+ return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+ int sockfd, numbytes;  
+ char buf[MAXDATASIZE];
+ struct addrinfo hints, *servinfo, *p;
+ int rv;
+ char s[INET6_ADDRSTRLEN];
+
+
+ char *full_url = argv[1];
+ // char *port_number= "80";
+ // char *full_path;
+ char full_request[MAXDATASIZE];
+ char *start_of_path = NULL;
+ char *start_of_port = NULL;
+ char *hostname_start = NULL;
+
+  int no_port_flag = 0;
+
+
+  size_t hostname_length;
+  size_t port_length;
+  size_t path_length;
+  char *hostname;
+  char *path;
+  char *port;
+
+
+ // checking for valid URL
+ char *start = full_url;
+ while(strncmp(start, "http://", 7) != 0){
+     if(*start == '\0'){
+         fprintf(stderr, "Invalid \n");
+         exit(1);
+     }
+     start++;
+ }
+
+ start = start + 7;
+ // printf("Start is %s \n", start);
+ 
+ // get the start of port number
+ start_of_port = start;
+ while(*start_of_port != ':'){
+     if(*start_of_port == '\0'){
+         // fprintf(stderr, "Port not found\n");
+         // exit(1);
+          // printf("No port in this request \n");
+          no_port_flag = 1;
+          start_of_port = start;
+          break;
+     }
+     start_of_port++;
+ }
+  // printf("Start of port is %s \n", start_of_port);
+
+
+ // get the start of file path
+ start_of_path = start_of_port;
+ while(*start_of_path != '/'){
+     if(*start_of_path == '\0'){
+          fprintf(stderr, "Path not found\n");
+          exit(1);
+          // printf("No port in this request 2 \n");
+          // no_port_flag = 1;
+          // start_of_path = start;
+          // break;
+     }
+     start_of_path++;
+
+ }
+  // printf("Start of path is %s \n", start_of_path);
+
+ hostname_start = start;
+
+   printf("no_port_flag: %d \n", no_port_flag);
+
+
+  if(no_port_flag == 1){
+
+        printf("HERE NO PORT! \n");
+
+      hostname_length = start_of_path - hostname_start;
+
+
+      port_length = 0;
+
+
+      path_length = strlen(start_of_path);
+
+
+      hostname = malloc(hostname_length + 1);
+ 
+      path = malloc(path_length + 1);
+
+
+      strncpy(hostname, hostname_start, hostname_length);
+      hostname[hostname_length] = '\0';
+
+
+      port = PORT;        // ???
+
+
+      strncpy(path, start_of_path + 1, path_length - 1);
+      path[path_length - 1] = '\0';
+
+  }
+  else{
+
+      hostname_length = start_of_port - hostname_start;
+      
+      port_length = start_of_path - start_of_port;
+
+      path_length = strlen(start_of_path);
+
+      hostname = malloc(hostname_length + 1);
+
+      port = malloc(port_length + 1);
+ 
+      path = malloc(path_length + 1);
+
+      strncpy(hostname, hostname_start, hostname_length);
+      hostname[hostname_length] = '\0';
+
+      strncpy(port, start_of_port + 1, port_length - 1); 
+      port[port_length - 1] = '\0';
+
+      strncpy(path, start_of_path + 1, path_length - 1);
+      path[path_length - 1] = '\0';
+
+
+  }
+
+
+ // Print extracted hostname, port, and path
+ printf("Hostname: %s\n", hostname);
+ printf("Port: %s\n", port);
+ printf("Path: %s\n", path);
+
+
+ if (argc != 2) {
+     fprintf(stderr,"usage: client hostname\n");
+     exit(1);
+ }
+
+ memset(&hints, 0, sizeof hints);
+ hints.ai_family = AF_UNSPEC;
+ hints.ai_socktype = SOCK_STREAM;
+
+
+
+
+ if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
+     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+     printf("error with port or hostname!");
+     return 1;
+ }
+
+
+ // loop through all the results and connect to the first we can
+ // printf("Trying to connect... \n");
+ for(p = servinfo; p != NULL; p = p->ai_next) {
+     if ((sockfd = socket(p->ai_family, p->ai_socktype,
+             p->ai_protocol)) == -1) {
+         perror("client: socket");
+         continue;
+     }
+
+     if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+         close(sockfd);
+         perror("client: connect");
+         continue;
+     }
+
+     // printf("Connected succesfully! \n");
+     break;
+ }
+
+
+ if (p == NULL) {
+     fprintf(stderr, "client: failed to connect\n");
+     return 2;
+ }
+
+
+ inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+         s, sizeof s);
+ printf("client: connecting to %s\n", s);
+
+
+ freeaddrinfo(servinfo); // all done with this structure
+
+
+ // create the GET request
+ if(path[0] == '/'){
+     // for absolute paths
+      if(no_port_flag == 1){
+          sprintf(full_request, "GET /%s HTTP/1.1\r\n\r\n", path);
+          printf("Path: %s \n", path);
+      }
+      else{
+          sprintf(full_request, "GET /%s HTTP/1.1\r\nHost: %s:%s\r\nConnection: closed\r\n\r\n", path, hostname, port);
+            printf("Path: %s \n", path);
+      }
+
+
+ }
+ else{
+
+      if(no_port_flag == 1){
+          sprintf(full_request, "GET %s HTTP/1.1\r\n\r\n", path);
+          printf("Path: %s \n", path);
+      }
+      else{
+          sprintf(full_request, "GET %s HTTP/1.1\r\nHost: %s:%s\r\nConnection: closed\r\n\r\n", path, hostname, port);
+            printf("Path: %s \n", path);
+      }
+
+
+
+
+ }
+
+ // send request and check for error
+ if(send(sockfd, full_request, strlen(full_request), 0) == -1){
+
+     perror("send");
+
+     close(sockfd);
+
+     exit(1);
+
+
+ }
+
+
+ FILE *file = fopen("output", "w");
+ if(!file){
+
+     perror("Error with output file");
+
+     close(sockfd);
+
+     exit(1);
+
+ }
+
+
+/*
+ int flag = 0;
+ int full_length = -1;
+ while((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) > 0){
+
+    // check if numbytes is 0
+    // check if fwrite is writing anything 
+
+     // buf[numbytes] = '\0';
+
+     if(!flag){
+         char *temp = strstr(buf, "\r\n\r\n");
+
+
+         if(temp != NULL){
+
+             flag = 1;
+
+             temp = temp + 4;
+
+             char *length = strstr(buf, "Content-Length:");
+             if(length != NULL){
+                 full_length = atoi(length + strlen("Content-Length:"));
+
+             }
+
+             if(full_length > 0){
+                 fwrite(temp, 1, full_length, file);
+                 memset(buf,'\0', MAXDATASIZE);
+                 full_length -= numbytes - (temp - buf);
+             }
+             else{
+                printf("Writing here!!!\n");
+
+                fwrite(temp, 1, numbytes - (temp - buf) - 4, file);
+                // fwrite(temp, 1, numbytes, file);
+                memset(buf,'\0', MAXDATASIZE);
+
+             }
+         }
+     }
+     else{
+       
+         if(full_length > 0){
+             fwrite(buf, 1, numbytes, file);
+             memset(buf,'\0', MAXDATASIZE);
+             full_length = full_length - numbytes;
+         }
+         else{
+             fwrite(buf, 1, numbytes, file);
+             memset(buf,'\0', MAXDATASIZE);
+         }
+     }
+     
+ }
+*/
+
+    int flag = 0;
+    char *phrase_1 = "HTTP/1.1 200 OK\r\n\r\n";
+    char *phrase_2 = "HTTP/1.1 400 Bad Request\r\n\r\n";
+    char *phrase_3 = "HTTP/1.1 404 File Not Found\r\n\r\n";
+    char *position_1;
+    char *position_2;
+    char *position_3;
+
+    while((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) > 0) {
+        
+        position_1 = strstr(buf, phrase_1);
+        position_2 = strstr(buf, phrase_2);
+        position_3 = strstr(buf, phrase_3);
+
+        if(position_1 != NULL){
+
+            flag = fwrite(buf, 1, position_1 - buf, file);
+
+            printf("Skipped phrase 1\n");
+
+            if(flag != (position_1 - buf)){
+                printf("ERROR 1 \n");
+                printf("number of bytes written: %d \n", flag);
+                printf("number of bytes recieved: %d \n", numbytes);
+                printf("error with fwrite! \n");
+                break;
+            }
+
+            // numbytes = numbytes - (position - buf) + strlen(phrase_1);
+
+            // memmove(buf, position + strlen(phrase_1), numbytes);
+
+            flag = fwrite(position_1 + strlen(phrase_1), 1, numbytes - (position_1 - buf) - strlen(phrase_1), file);
+
+            if(flag != (numbytes - (position_1 - buf) - strlen(phrase_1))){
+                printf("ERROR 2\n");
+                printf("number of bytes written: %d \n", flag);
+                printf("number of bytes recieved: %d \n", numbytes);
+                printf("error with fwrite! \n");
+                break;
+            }
+
+        }
+        else if(position_2 != NULL){
+
+            flag = fwrite(buf, 1, position_2 - buf, file);
+
+            printf("Skipped phrase 2 \n");
+
+            if(flag != (position_2 - buf)){
+                printf("ERROR 1 \n");
+                printf("number of bytes written: %d \n", flag);
+                printf("number of bytes recieved: %d \n", numbytes);
+                printf("error with fwrite! \n");
+                break;
+            }
+
+            // numbytes = numbytes - (position - buf) + strlen(phrase_1);
+
+            // memmove(buf, position + strlen(phrase_1), numbytes);
+
+            flag = fwrite(position_2 + strlen(phrase_2), 1, numbytes - (position_2 - buf) - strlen(phrase_2), file);
+
+            if(flag != (numbytes - (position_2 - buf) - strlen(phrase_2))){
+                printf("ERROR 2\n");
+                printf("number of bytes written: %d \n", flag);
+                printf("number of bytes recieved: %d \n", numbytes);
+                printf("error with fwrite! \n");
+                break;
+            }
+
+        }
+        else if(position_3 != NULL){
+
+            break;
+
+            flag = fwrite(buf, 1, position_3 - buf, file);
+
+            printf("Skipped phrase 3\n");
+
+            if(flag != (position_3 - buf)){
+                printf("ERROR 1 \n");
+                printf("number of bytes written: %d \n", flag);
+                printf("number of bytes recieved: %d \n", numbytes);
+                printf("error with fwrite! \n");
+                break;
+            }
+
+            // numbytes = numbytes - (position - buf) + strlen(phrase_1);
+
+            // memmove(buf, position + strlen(phrase_1), numbytes);
+
+            flag = fwrite(position_3 + strlen(phrase_3), 1, numbytes - (position_3 - buf) - strlen(phrase_3), file);
+
+            if(flag != (numbytes - (position_3 - buf) - strlen(phrase_3))){
+                printf("ERROR 3\n");
+                printf("number of bytes written: %d \n", flag);
+                printf("number of bytes recieved: %d \n", numbytes);
+                printf("error with fwrite! \n");
+                break;
+            }
+
+        }
+        else{
+            // printf("No skip needed \n");
+            flag = fwrite(buf, 1, numbytes, file);
+
+            if(flag != numbytes){
+                printf("ERROR \n");
+                printf("number of bytes written: %d \n", flag);
+                printf("number of bytes recieved: %d \n", numbytes);
+                printf("error with fwrite! \n");
+                break;
+            }
+        }
+
+        //printf("number of bytes written: %d \n", flag);
+        //printf("number of bytes recieved: %d \n", numbytes);
+
+        memset(buf, '\0', MAXDATASIZE);
+    }
+
+
+
+
+ if(numbytes < 0){
+
+     perror("recv");
+
+     close(sockfd);
+
+     fclose(file);
+
+     exit(1);
+
+ }
+
+
+ // printf("Saved to output file \n");
+ close(sockfd);
+
+ fclose(file);
+
+ free(hostname);
+
+  if(no_port_flag != 1){
+      free(port);
+  }
+
+ free(path);
+
+ return 0;
+}
---
+++ src/http_server.c
@@ -0,0 +1,275 @@
+/*
+** server.c -- a stream socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+
+
+
+#define PORT "80"       // the port users will be connecting to
+
+#define BACKLOG 20      // how many pending connections queue will hold
+
+#define BUFFER 5000     // size of the buffers to send and recieve data
+
+
+void sigchld_handler(int s)
+{
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char* argv[])
+{
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+
+    char request[BUFFER];       // buffer to hold the request
+
+    char response[BUFFER];      // buffer to hold the response
+
+    int bytes;                  // number of bytes
+
+    char *port;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+
+    port = argv[1];
+
+    printf("Port: %s \n", port);
+
+    if(port == NULL){
+
+        port = PORT;
+
+    }
+
+    if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    printf("server: waiting for connections...\n");
+
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        printf("server: got connection from %s\n", s);
+
+        memset(request, '\0', BUFFER);      // setup request buffer with NULL characters
+
+
+        if (!fork()) { // this is the child process
+            close(sockfd); // child doesn't need the listener
+
+            recv(new_fd, request, BUFFER, 0);       // recieves message and stores it into request buffer
+    
+            char *request_ = request;       // get pointer to each char in request
+
+            printf("Original request: %s", request_);
+            
+            int temp = 0;                   // init temp variable
+
+            // loop through until a NULL or space character is found and increment temp
+            while (request_[temp] != '\0' && request_[temp] != ' ') {
+               
+               temp++;
+           
+            }
+
+            // check for a get request
+            if (strncmp(request_, "GET /", 5) != 0) {
+               strcpy(response, "HTTP/1.1 400 Bad Request\r\n\r\n");
+               if (send(new_fd, response, strlen(response), 0) == -1) {
+                   perror("send");
+               }
+               exit(1);
+            }
+
+            printf("Full Request: %s", request);
+            printf("Full Request: %s", request_);
+
+            request_ = request + 5;    // skip 4 characters in request 
+
+            printf("Full Request: %s", request_);
+
+            temp = 0;                    // reset temp counter
+
+            // loop through until a NULL or space character is found and increment temp
+            while (request_[temp] != '\0' && request_[temp] != ' ') {
+               
+               temp++;
+           
+            }
+
+
+
+            if (request_[temp] == '\0') {
+               strcpy(response, "HTTP/1.1 400 Bad Request\r\n\r\n");
+               if (send(new_fd, response, strlen(response), 0) == -1) {
+                   perror("send");
+               }
+               exit(1);
+            }
+
+
+            char file_path[BUFFER];
+
+            strncpy(file_path, request_, temp);
+
+            printf("First char in path: %c \n", file_path[0]);
+
+            printf("File path: %s \n", file_path);
+
+            file_path[temp] = '\0';
+
+            printf("First char in path: %c \n", file_path[0]);
+
+            printf("File path: %s \n", file_path);
+
+
+            // printf("Requested file: %s\n", file_path);      // print path for debugging
+
+            FILE *file = fopen(file_path, "rb");
+            // not getting correct file path here?!?!?!
+            // /home/fulcrum/Desktop/sp24_cs438_.release/mp1/teamname.txt
+            // needs to account for relative path and absolute path!
+            // if relative path, there should only be 1 /
+
+
+            if (!file) {
+               strcpy(response, "HTTP/1.1 404 File Not Found\r\n\r\n");
+               // printf("404 ERROR HERE!!!");
+               if (send(new_fd, response, strlen(response), 0) == -1) {
+                   perror("send");
+               }
+               perror("fopen");
+               exit(1);
+            }
+
+
+            // file is found so we send 200 OK message
+            strcpy(response, "HTTP/1.1 200 OK\r\n\r\n");
+            if (send(new_fd, response, strlen(response), 0) == -1) {
+               perror("send");
+               exit(1);
+            } 
+
+            // send contents of file
+            memset(response, '\0', BUFFER);         // fill response buffer with NULL characters
+
+            /*
+            while ((bytes = fread(response, sizeof(char), BUFFER, file)) != 0) {
+               if (send(new_fd, response, bytes, 0) == -1) {
+                   perror("send");
+                   exit(1);
+               }
+               memset(response, '\0', BUFFER);
+            }
+            */
+           
+           size_t bytes_read;
+           while ((bytes_read = fread(response, 1, BUFFER, file)) > 0) {
+               if (send(new_fd, response, bytes_read, 0) == -1) {
+                   perror("send");
+                   //exit(1);
+                   break;
+               }
+               memset(response, '\0', BUFFER);
+            }
+
+
+            // if (send(new_fd, "Hello, world!", 13, 0) == -1)
+            //  perror("send");
+
+            fclose(file);
+            close(new_fd);
+            exit(0);
+        }
+        close(new_fd);  // parent doesn't need this
+    }
+
+    return 0;
+}
